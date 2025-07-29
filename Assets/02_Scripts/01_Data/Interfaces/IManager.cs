49e4a0985d6eb4d850a6295bf6e1edd02e12f1a3
abcd8197 <abcd8197@naver.com>
2025-07-30 00:18:19 +0900

    github #2 Main 스크립트 추가

---
+++ Assets/02_Scripts/01_Data/Interfaces/IManager.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace CodeJay
+{
+    public interface IManager : IDisposable
+    {
+
+    }
+}
--- Assets/02_Scripts/02_Core/Managers/Main.cs
+++ Assets/02_Scripts/02_Core/Managers/Main.cs
@@ -1,10 +1,65 @@
 using System;
 using System.Collections.Generic;
 
-namespace Codejay
+namespace CodeJay
 {
-    public class Main
+    public class Main : IDisposable
     {
-        
+        private const string ManagerNotRegisteredError = "Manager of type \"{0}\" is not registered.";
+
+        public Main Instance => Lazy.Value;
+        private static readonly Lazy<Main> Lazy = new(() => new Main());
+
+        private Dictionary<Type, IManager> _managers = new();
+
+        public void RegisterManager<T>(T manager) where T : IManager
+        {
+            if (manager == null)
+                throw new ArgumentNullException(nameof(manager));
+
+            var type = typeof(T);
+
+            if (_managers.ContainsKey(type))
+                throw new InvalidOperationException(string.Format(ManagerNotRegisteredError, type.Name));
+
+            _managers[type] = manager;
+        }
+
+        public void UnregisterManager<T>() where T : IManager
+        {
+            var type = typeof(T);
+
+            if (!_managers.ContainsKey(type))
+                throw new InvalidOperationException(string.Format(ManagerNotRegisteredError, type.Name));
+
+            _managers[type].Dispose();
+            _managers.Remove(type);
+        }
+
+        public T Get<T>() where T : IManager, new()
+        {
+            var type = typeof(T);
+
+            if (!_managers.TryGetValue(type, out var manager))
+                throw new InvalidOperationException(string.Format(ManagerNotRegisteredError, type.Name));
+
+            return (T)manager;
+        }
+
+
+        public void Clear()
+        {
+            foreach (var manager in _managers.Values)
+            {
+                manager.Dispose();
+            }
+
+            _managers.Clear();
+        }
+
+        public void Dispose()
+        {
+            Clear();
+        }
     }
 }
